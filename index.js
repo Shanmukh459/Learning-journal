8ff827cd4215f454e10f4c7976cec3989de1f49a
Shanmukh459 <saikrrish459@gmail.com>
2024-01-09 14:35:10 -0800

    about page with out posts

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ document.addEventListener('click', function(e) {
     if(e.target.dataset.home) {
         renderHomePage()
     }
+    else if(e.target.dataset.about) {
+        console.log(e.target.dataset.about)
+        renderAboutPage()
+    }
 })
 mainSection.innerHTML = getIndexHtml()
 
 function renderHomePage() {
     mainSection.innerHTML = getHomePageHeroHtml() + `<h3 class="post-heading">Recent posts</h3>` + postsHtml
+    console.log(mainSection.innerHTML)
+}
+
+function renderAboutPage() {
+    console.log("mainsection before render about", mainSection.innerHTML)
+    mainSection.innerHTML = getAboutPageHeroHtml()
+    console.log("render about executed!", mainSection.innerHTML)
+
 }
 
 function getIndexHtml() {
@@ -40,6 +52,22 @@ function getHomePageHeroHtml() {
     </section>
         `
 
+}
+
+function getAboutPageHeroHtml() {
+    console.log("Inside about Html method")
+    return `
+        <section class="about-hero">
+            <img src="images/profile-icon.jpg">
+            <h1>Hi there! My name is Shanmukh and welcome to my learning journal.</h1>
+            <p class="about hero-desc">After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.</p>
+            <h3>How I stay committed to learning</h3>
+            <p>I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.</p>
+            <p>While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.</p>
+            <h3>How I got started</h3>
+            <p>I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.</p>
+            <p>That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.</p>
+        </section>`
 }
 
 function getPostsHtml() {
